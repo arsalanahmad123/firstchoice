7df3113a1775a6e5807813daa8ef61d370fb4040
waris64 <warismuhammad652@gmail.com>
2024-03-29 22:29:24 +0500

    cards added in DB

--- src/Components/DashboardCard.jsx
+++ src/Components/DashboardCard.jsx
@@ -9,43 +9,43 @@ const DashboardCard = () => {
     return (
         <>
             <div className='flex flex-row justify-center  items-center py-1 gap-x-5 w-full'>
-                <div className="card w-72 bg-bgLight text-white ">
+                <div className="card w-72 bg-[#d2eff3] black ">
                     <MdGroupWork className='size-10 lg:ml-60' />
                     <div className="card-body pt-">
                         <h2 className="text-md -mt-8">Active Works</h2>
                         <p>$$$</p>
-                        
+
                     </div>
                 </div>
-                <div className="card w-72 bg-bgLight text-white">
+                <div className="card w-72 bg-[#fffac3] text-black">
                     <HiBuildingOffice2 className='size-10 lg:ml-60' />
                     <div className="card-body pt-">
-                        <h2 className="text-md -mt-8">Total 
-Companies</h2>
+                        <h2 className="text-md -mt-8">Total
+                            Companies</h2>
                         <p>$$$</p>
-                        
+
                     </div>
                 </div>
-
-
-            </div>
-            <div className='flex flex-row justify-center  items-center py-5 gap-x-5 w-full'>
-            <div className="card w-72 bg-bgLight text-white">
+                <div className="card w-72 bg-[#fffac3] text-black">
                     <FaSortAmountUp className='size-9 lg:ml-60' />
                     <div className="card-body pt-">
                         <h2 className=" text-md -mt-8">Daily Revenue</h2>
                         <p>$$$</p>
-                        
+
                     </div>
                 </div>
-                <div className="card w-72 bg-bgLight text-white">
+                <div className="card w-72 bg-[#ffccbe] text-black">
                     <GiProfit className='size-10 lg:ml-60 ' />
                     <div className="card-body pt-">
                         <h2 className="text-md -mt-8">Daily Profit</h2>
                         <p>$$$</p>
-                        
+
                     </div>
                 </div>
+
+            </div>
+            <div className='flex flex-row justify-center  items-center py-5 gap-x-5 w-full'>
+
 
             </div>
 
--- src/Screens/Dashboard.jsx
+++ src/Screens/Dashboard.jsx
@@ -7,15 +7,15 @@ const Dashboard = () => {
     return (
         <>
             <Wrapper title={'Dashboard'}>
-                <div className='flex justify-between items-start px-5 rounded-md mt-1 gap-x-2'>
-                    <div className='flex flex-col justify-start items-start gap-x-4 w-[65%] min-h-screen'>
+                <div className='flex justify-between items-start px-5 rounded-md mt-1 gap-x-2 '>
+                    <div className='flex flex-col justify-start items-start gap-x-4  min-h-screen w-full'>
                         <DashboardCard />
                         <div className='w-full bg-bgLight flex justify-center items-center p-10'>
                             {/* <Donut className="-lg:ml-9 "/> */}
                              <h1 className="text-white">Chart will be here</h1> 
                         </div>
                     </div>
-                    <div className='flex flex-col justify-center items-center w-[35%] min-h-screen bg-bgLight'></div>
+                   
                 </div>
             </Wrapper>
         </>
