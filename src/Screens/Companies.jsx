04c28e267b42027163f2b3b0bc3ab75cd8b98704
waris64 <warismuhammad652@gmail.com>
2024-03-29 15:58:06 +0500

    add invoice form is ready

--- src/Screens/Companies.jsx
+++ src/Screens/Companies.jsx
@@ -8,22 +8,12 @@ import Ellipse22 from '../assets/Ellipse22.png'
 import Ellipse24 from '../assets/Ellipse24.png'
 import { CgSearch } from "react-icons/cg";
 import EmployeeProfile from "../Components/EmployeeProfile";
+import Header from "../Components/Header";
 const Companies = () => {
   return (
     <>
       <Wrapper title={"Companies"}>
-        <div className="flex  justify-between  gap-x-20 lg:pt-4 px-5">
-          <div className="relative w-full">
-            <input
-              type="text"
-              className="w-full lg:py-1 pl-5 lg:rounded-2xl bg-bgLight border-2 border-gray-700 text-white"
-            />
-            <CgSearch className="text-slate-700 m-auto absolute lg:right-5 lg:top-3  " />
-          </div>
-          <button className="text-white text-sm w-52 px-2 lg:py-1 lg:rounded-2xl bg-gradient-to-b from-yellow-400 to-yellow-600">
-            Add New Company
-          </button>
-        </div>
+       <Header/>
         <div className="flex">
         <div className="flex gap-x-7 px-6   pt-16 text-center">
           <EmployeeProfile img={Ellipse} desc={"Ahad"} />
--- src/Screens/Invoice.jsx
+++ src/Screens/Invoice.jsx
@@ -2,13 +2,32 @@ import React from 'react'
 import AppLayout from '../Layout/AppLayout'
 import Wrapper from '../Layout/Wrapper'
 import Table from '../Components/Table'
+import Header from '../Components/Header'
+import { CgSearch } from 'react-icons/cg'
 
 const Invoice = () => {
     return (
         <>
             <Wrapper title={'Invoice'}>
                 <h1 className='text-white mt-3'>Our Latest Invoices </h1>
-               <Table />
+
+                <div className="flex  justify-between items-center  gap-x-20 lg:pt-4 px-5">
+                    <div className="relative">
+                        <input
+                            type="text"
+                            className="w-full lg:py-1 pl-5 lg:rounded-2xl bg-bgLight border-2 border-gray-700 text-white"
+                        />
+                        <CgSearch className="text-slate-700 m-auto absolute lg:right-5 lg:top-3  " />
+                    </div>
+                    <div className='flex justify-center items-center gap-x-3'>
+                    <span className='badge badge-warning py-3 cursor-pointer  '>pending</span>
+                    <span className='badge badge-success py-3  cursor-pointer'>completed</span>
+                    </div>
+                    <button className="text-white text-sm w-52 px-2 lg:py-1 lg:rounded-2xl bg-gradient-to-b from-yellow-400 to-yellow-600">
+                        Add New Company
+                    </button>
+                </div>
+                <Table />
             </Wrapper>
         </>
     )
