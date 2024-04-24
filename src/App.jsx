ab6c388720af60f1d716ad4c5a0b4b7ab33a9e96
arsalanahmad123 <arsalanahmad15092003@gmail.com>
2024-04-24 16:09:23 +0500

    removed lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,40 @@
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { lazy, Suspense, useState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import ProtectedRoute from './Layout/ProtectedRoute'
-const Dashboard = lazy(() => import('./Screens/Dashboard'))
-const Login = lazy(() => import('./Screens/Login'))
-const Companies = lazy(() => import('./Screens/Companies'))
-const Orders = lazy(() => import('./Screens/Orders'))
-const Work = lazy(() => import('./Screens/Work'))
-const Invoice = lazy(() => import('./Screens/Invoice'))
-const Expense = lazy(() => import('./Screens/Expense'))
-const Revenue = lazy(() => import('./Screens/Revenue'))
-const Services = lazy(() => import('./Screens/Services'))
-const Admin = lazy(() => import('./Screens/Admin'))
-const AddInvoice = lazy(() => import('./Screens/AddInvoice'))
-import Loader from './Components/Loader'
+import Dashboard from './Screens/Dashboard'
+import Login from './Screens/Login'
+import Companies from './Screens/Companies'
+import Orders from './Screens/Orders'
+import Work from './Screens/Work'
+import Invoice from './Screens/Invoice'
+import Expense from './Screens/Expense'
+import Revenue from './Screens/Revenue'
+import Services from './Screens/Services'
+import Admin from './Screens/Admin'
+import AddInvoice from './Screens/AddInvoice'
 import { Toaster } from 'react-hot-toast'
 
 function App() {
-    const [loading, setLoading] = useState(true)
-
-    useEffect(() => {
-        const timer = setTimeout(() => {
-            setLoading(false)
-        }, 5000)
-        return () => clearTimeout(timer)
-    }, [])
     return (
         <>
             <BrowserRouter>
-                <Suspense fallback={loading && <Loader />}>
-                    <Toaster />
-                    <Routes>
-                        <Route element={<ProtectedRoute />}>
-                            <Route path='/' element={<Dashboard />} />
-                            <Route
-                                path='/companies/*'
-                                element={<Companies />}
-                            />
-                            <Route path='/orders' element={<Orders />} />
-                            <Route path='/work' element={<Work />} />
-                            <Route path='/invoice/*' element={<Invoice />} />
-                            <Route path='/expense' element={<Expense />} />
-                            <Route path='/revenue' element={<Revenue />} />
-                            <Route path='/services' element={<Services />} />
-                            <Route
-                                path='/add_invoice'
-                                element={<AddInvoice />}
-                            />
-                        </Route>
-                        <Route path='/login' element={<Login />} />
-                        <Route path='/admin' element={<Admin />} />
-                    </Routes>
-                </Suspense>
+                <Toaster />
+                <Routes>
+                    <Route element={<ProtectedRoute />}>
+                        <Route path='/' element={<Dashboard />} />
+                        <Route path='/companies/*' element={<Companies />} />
+                        <Route path='/orders' element={<Orders />} />
+                        <Route path='/work' element={<Work />} />
+                        <Route path='/invoice/*' element={<Invoice />} />
+                        <Route path='/expense' element={<Expense />} />
+                        <Route path='/revenue' element={<Revenue />} />
+                        <Route path='/services' element={<Services />} />
+                        <Route path='/add_invoice' element={<AddInvoice />} />
+                    </Route>
+                    <Route path='/login' element={<Login />} />
+                    <Route path='/admin' element={<Admin />} />
+                </Routes>
             </BrowserRouter>
         </>
     )
